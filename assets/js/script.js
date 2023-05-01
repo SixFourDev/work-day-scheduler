ec554c4c55cfe444e6f8c7168b88d986d34db8b5
Shane <sb_94@yahoo.com>
2023-05-01 10:56:14 -0700

    added function for timeBlockColor

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 });
 
 $(function () {
-  
+
   function userEntry() {
     // Retrieve user input from text area and save to local storage on button click
     $(".saveBtn").on("click", function () {
@@ -18,23 +18,45 @@ $(function () {
       localStorage.setItem(timeBlockId, userInput);
     });
   }
-  
+
   userEntry();
+
+  function timeBlockColor() {
+    // Creates a variable for now with the current date
+    var now = dayjs();
+    // Creates a variable for currentHour and gets the day with now, and hour() to get the current hour.
+    var currentHour = now.hour();
+
+    // Selects the class "time-block, and uses the each method to iterate over each of them"
+    $(".time-block").each(function () {
+      // Gets id from the current "time-block" element by using split to take away the dash
+      // in hour, and selecting the hour part. parseInt is used to convert the hour string to an integer.
+      var blockHour = parseInt(this.id.split("-")[1]);
+      // "this" line toggles the class "past" on the current time block
+      $(this).toggleClass('past', blockHour < currentHour);
+      // "this" line toggles the class "present" on the current time block
+      $(this).toggleClass('present', blockHour === currentHour);
+      // "this" line toggles the "future" on the current tike block
+      $(this).toggleClass('future', blockHour > currentHour);
+    });
+  }
+  // Calls the timeBlockColor function
+  timeBlockColor();
 });
 
 
 
 
 
-  //   TODO: Add code to apply the past, present, or future class to each time
-  //   block by comparing the id to the current hour. HINTS: How can the id
-  //   attribute of each time-block be used to conditionally add or remove the
-  //   past, present, and future classes? How can Day.js be used to get the
-  //   current hour in 24-hour time?
+//   TODO: Add code to apply the past, present, or future class to each time
+//   block by comparing the id to the current hour. HINTS: How can the id
+//   attribute of each time-block be used to conditionally add or remove the
+//   past, present, and future classes? How can Day.js be used to get the
+//   current hour in 24-hour time?
 
-  //   TODO: Add code to get any user input that was saved in localStorage and set
-  //   the values of the corresponding textarea elements. HINT: How can the id
-  //   attribute of each time-block be used to do this?
+//   TODO: Add code to get any user input that was saved in localStorage and set
+//   the values of the corresponding textarea elements. HINT: How can the id
+//   attribute of each time-block be used to do this?
 
 
 
