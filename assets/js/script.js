f2a562447df0f893f911c0a0b9995b2697cd2d05
Shane <sb_94@yahoo.com>
2023-05-01 13:39:13 -0700

    cleaned up js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,42 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(document).ready(function () {
   var userInput = $(".time-block").val();
 });
 
-$(function () {
+function userEntry() {
+  // Retrieve user input from text area and save to local storage on button click
+  $(".saveBtn").on("click", function () {
+    // Gets description from "this" sibling 
+    var userInput = $(this).siblings(".description").val();
+    // Gets id of "this" time-block, which contains the hour value
+    var timeBlockId = $(this).closest(".time-block").attr("id");
+    // Saves userInput from the timeBlockId to the localStorage
+    localStorage.setItem(timeBlockId, userInput);
+  });
+}
+userEntry();
 
-  function userEntry() {
-    // Retrieve user input from text area and save to local storage on button click
-    $(".saveBtn").on("click", function () {
-      // Gets description from "this" sibling 
-      var userInput = $(this).siblings(".description").val();
-      // Gets id of "this" time-block, which contains the hour value
-      var timeBlockId = $(this).closest(".time-block").attr("id");
-      // Saves userInput from the timeBlockId to the localStorage
-      localStorage.setItem(timeBlockId, userInput);
-    });
-  }
-  userEntry();
+function timeBlockColor() {
+  // Creates a variable for now with the current date
+  var now = dayjs();
+  // Creates a variable for currentHour and gets the day with now, and hour() to get the current hour.
+  var currentHour = now.hour();
 
-  function timeBlockColor() {
-    // Creates a variable for now with the current date
-    var now = dayjs();
-    // Creates a variable for currentHour and gets the day with now, and hour() to get the current hour.
-    var currentHour = now.hour();
+  // Selects the class "time-block, and uses the each method to iterate over each of them"
+  $(".time-block").each(function () {
+    // Gets id from the current "time-block" element by using split to take away the dash
+    // in hour, and selecting the hour part. parseInt is used to convert the hour string to an integer.
+    var blockHour = parseInt(this.id.split("-")[1]);
+    // "this" line toggles the class "past" on the current time block and sets it less than the current hour
+    $(this).toggleClass('past', blockHour < currentHour);
+    // "this" line toggles the class "present" on the current time block and is equal to the current hour
+    $(this).toggleClass('present', blockHour === currentHour);
+    // "this" line toggles the "future" on the current tike block and is greater than the current hour
+    $(this).toggleClass('future', blockHour > currentHour);
+  });
+}
+// Calls the timeBlockColor function
+timeBlockColor();
 
-    // Selects the class "time-block, and uses the each method to iterate over each of them"
-    $(".time-block").each(function () {
-      // Gets id from the current "time-block" element by using split to take away the dash
-      // in hour, and selecting the hour part. parseInt is used to convert the hour string to an integer.
-      var blockHour = parseInt(this.id.split("-")[1]);
-      // "this" line toggles the class "past" on the current time block and sets it less than the current hour
-      $(this).toggleClass('past', blockHour < currentHour);
-      // "this" line toggles the class "present" on the current time block and is equal to the current hour
-      $(this).toggleClass('present', blockHour === currentHour);
-      // "this" line toggles the "future" on the current tike block and is greater than the current hour
-      $(this).toggleClass('future', blockHour > currentHour);
-    });
-  }
-  // Calls the timeBlockColor function
-  timeBlockColor();
-});
 
 function getSavedUserInput() {
   // Loop through each time-block
